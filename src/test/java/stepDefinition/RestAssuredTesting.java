03789d2ec7e496b240ef3981cb62b44ea7b93562
Pavithra Mugundhan <pavithramuthu27@gmail.com>
2022-05-22 20:59:56 -0400

    Update RestAssuredTesting.java
    check the remote

--- src/test/java/stepDefinition/RestAssuredTesting.java
+++ src/test/java/stepDefinition/RestAssuredTesting.java
@@ -36,7 +36,7 @@ public class RestAssuredTesting {
 	 RequestSpecification request=given();
 	 response= request.when().get(url);
 
-		  System.out.println(response.asPrettyString()+"new change updated 1");
+		  System.out.println(response.asPrettyString()+"new change updated remote");
 
 }
 
