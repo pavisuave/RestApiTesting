473728a54e7b85818bcb0dcd94e8161c8bfcf6cd
pavisuave <pavimuthu27@gmail.com>
2022-05-22 21:32:17 -0400

    message 1

--- src/test/java/stepDefinition/RestAssuredTesting.java
+++ src/test/java/stepDefinition/RestAssuredTesting.java
@@ -31,6 +31,7 @@ public class RestAssuredTesting {
 		RequestSpecification request = given();
 		response = request.when().get(url);
 		System.out.println(response.asPrettyString() + "new change update  nice add mugi here");
+		//new status add
 	}
 
 	@Then("Response is {string}")
